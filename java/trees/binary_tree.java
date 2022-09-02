cfbed8bd5dd918e10d85fcd557614fe8bd824a44
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-09-02 18:58:54 +0530

    completed array to tree and removed hello world

--- java/trees/binary_tree.java
+++ java/trees/binary_tree.java
@@ -23,27 +23,26 @@ class BinaryTree{
     static BinaryTree arrayToTree(int[] a){
         BinaryTree result = new BinaryTree();
 
-        Queue<Integer> pos = new LinkedList<>();
-        Queue<Node> nodes = new LinkedList<>();
+        Queue<Integer> pos = new LinkedList<>(); //to track next array element to be inserted into tree
+        Queue<Node> nodes = new LinkedList<>(); //to track current cursor position for insertion
 
         pos.add(0);
         result.root = result.new Node(a[0]);
         nodes.add(result.root);
         
-        while(pos.size()>0){
+        while(pos.size()>0){ //While queue is not empty
             int cp = pos.remove(), left = cp*2 + 1, right = cp*2 + 2; // cp: current position
             Node cursor = nodes.remove();
-            if(left < a.length){
+            if(left < a.length && a[left]!=-1){
                 cursor.left = result.new Node(a[left]);
                 pos.add(left);
                 nodes.add(cursor.left);
             }
-            if(right < a.length){
+            if(right < a.length && a[right]!=-1){
                 cursor.right = result.new Node(a[right]);
                 pos.add(right);
                 nodes.add(cursor.right);
             }
-            System.out.println("Current Position: " + cp);
         }
 
         return result;
@@ -58,8 +57,18 @@ class BinaryTree{
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int[] a = new int[]{1, 4, 6, 3, 4, 6};
+        int[] a = new int[]{1, -1, 2, -1, -1, 4, 3, -1, -1, -1, -1, 5};
+        /*
+                      root
+                        1
+
+                NULL         2
+
+                          4     3
+
+                       5
+         */
+                
         BinaryTree tree = arrayToTree(a);
         System.out.println("Printing tree in pre order traversal:");
         tree.dfs(tree.root);
